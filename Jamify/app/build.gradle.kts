079205797834e10dddbfac01eca7c1b3ec6fdc28
Shadin Hussein <shadinhussein@Shadins-MacBook-Pro.local>
2024-03-30 17:47:27 -0500

    build files

--- Jamify/app/build.gradle.kts
+++ Jamify/app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+//    kotlin("kapt") version "1.9.23"
+    id("kotlin-kapt")
+
+
 }
 
 android {
@@ -42,12 +46,27 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.storage.ktx)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     // TODO: Add the dependencies for Firebase products you want to use
-    // When using the BoM, don't specify versions in Firebase dependencies
-    implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-analytics")
+    // Declare the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation ("com.google.firebase:firebase-analytics-ktx")
+    implementation ("com.google.firebase:firebase-auth-ktx")
+    implementation ("com.google.firebase:firebase-firestore-ktx")
+    implementation ("com.google.firebase:firebase-storage-ktx")
+    implementation ("com.firebaseui:firebase-ui-auth:8.0.2")
+    implementation ("com.firebaseui:firebase-ui-storage:8.0.2")
+
+    // Glide
+//    kapt("groupId:artifactId:version")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    kapt ("com.github.bumptech.glide:compiler:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
 }
---
+++ Jamify/app/src/main/java/com/example/jamify/ViewModelDBHelper.kt
@@ -0,0 +1,116 @@
+package com.example.jamify
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+
+class ViewModelDBHelper() {
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val collectionRoot = "allNotes"
+
+    private fun elipsizeString(string: String) : String {
+        if(string.length < 10)
+            return string
+        return string.substring(0..9) + "..."
+    }
+
+    fun fetchInitialNotes(notesList: MutableLiveData<List<Note>>,
+                          callback:()->Unit) {
+        dbFetchNotes(notesList, callback)
+    }
+    /////////////////////////////////////////////////////////////
+    // Interact with Firestore db
+    // https://firebase.google.com/docs/firestore/query-data/get-data
+    //
+    // If we want to listen for real time updates use this
+    // .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+    // But be careful about how listener updates live data
+    // and noteListener?.remove() in onCleared
+    private fun dbFetchNotes(notesList: MutableLiveData<List<Note>>,
+                             callback:()->Unit = {}) {
+        db.collection(collectionRoot)
+            .orderBy("timeStamp", Query.Direction.DESCENDING)
+            .limit(100)
+            .get()
+            .addOnSuccessListener { result ->
+                Log.d(javaClass.simpleName, "allNotes fetch ${result!!.documents.size}")
+                // NB: This is done on a background thread
+                notesList.postValue(result.documents.mapNotNull {
+                    it.toObject(Note::class.java)
+                })
+                callback()
+            }
+            .addOnFailureListener {
+                Log.d(javaClass.simpleName, "allNotes fetch FAILED ", it)
+                callback()
+            }
+    }
+
+    // After we successfully modify the db, we refetch the contents to update our
+    // live data.  Hence the dbFetchNotes() calls below.
+    fun updateNote(
+        note: Note,
+        notesList: MutableLiveData<List<Note>>
+    ) {
+        val pictureUUIDs = note.pictureUUIDs
+        //SSS
+        db.collection(collectionRoot)
+            .document(note.firestoreID)
+            .set(note)
+            //EEE // XXX Writing a note
+            .addOnSuccessListener {
+                Log.d(
+                    javaClass.simpleName,
+                    "Note update \"${elipsizeString(note.text)}\" len ${pictureUUIDs.size} id: ${note.firestoreID}"
+                )
+                dbFetchNotes(notesList)
+            }
+            .addOnFailureListener { e ->
+                Log.d(javaClass.simpleName, "Note update FAILED \"${elipsizeString(note.text)}\"")
+                Log.w(javaClass.simpleName, "Error ", e)
+            }
+    }
+
+    fun createNote(
+        note: Note,
+        notesList: MutableLiveData<List<Note>>
+    ) {
+        // We can get ID locally
+        // note.firestoreID = db.collection("allNotes").document().id
+
+        db.collection(collectionRoot)
+            .add(note)
+            .addOnSuccessListener {
+                Log.d(
+                    javaClass.simpleName,
+                    "Note create \"${elipsizeString(note.text)}\" id: ${note.firestoreID}"
+                )
+                dbFetchNotes(notesList)
+            }
+            .addOnFailureListener { e ->
+                Log.d(javaClass.simpleName, "Note create FAILED \"${elipsizeString(note.text)}\"")
+                Log.w(javaClass.simpleName, "Error ", e)
+            }
+    }
+
+    fun removeNote(
+        note: Note,
+        notesList: MutableLiveData<List<Note>>
+    ) {
+        db.collection(collectionRoot)
+            .document(note.firestoreID)
+            .delete()
+            .addOnSuccessListener {
+                Log.d(
+                    javaClass.simpleName,
+                    "Note delete \"${elipsizeString(note.text)}\" id: ${note.firestoreID}"
+                )
+                dbFetchNotes(notesList)
+            }
+            .addOnFailureListener { e ->
+                Log.d(javaClass.simpleName, "Note deleting FAILED \"${elipsizeString(note.text)}\"")
+                Log.w(javaClass.simpleName, "Error adding document", e)
+            }
+    }
+}
